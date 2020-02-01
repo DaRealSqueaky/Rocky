4c40de77a4b9dfe66801408b10f6e033b277d2dc
WatTech <watsonstechnologyofficial@hotmail.com>
2020-02-01 15:45:53 +0000

    Added more cool messages for when you win or lose :)

--- Rocky.py
+++ Rocky.py
@@ -13,24 +13,25 @@ while player == False:
 #set player to True
     player = input("Rock, Paper, Scissors?")
     if player == computer:
-        print("Oh wow! You tied :P")
+        print("Tie!")
     elif player == "Rock":
         if computer == "Paper":
-            print("You lose! Even Squeaky could beat me :)")", computer, "covers", player)
+            print("You lose! Victor Tran has turned from a blue happy face into a red angry face!!!", computer, "covers", player)
         else:
-            print("You win!", player, "smashes", computer)
+            print("You win and #youwonwithrock!", player, "smashes", computer)
     elif player == "Paper":
         if computer == "Scissors":
-            print("You lose! Even Squeaky could beat me :)", computer, "cut", player)
+            print("You lose! Even Victor Tran could beat this game!", computer, "cut", player)
         else:
             print("You win!", player, "covers", computer)
     elif player == "Scissors":
         if computer == "Rock":
-            print("You lose! Even Squeaky could beat me :)", computer, "smashes", player)
+            print("You lose! Even Victor Tran could beat this game!", computer, "smashes", player)
         else:
             print("You win!", player, "cut", computer)
     else:
-        print("I'm sorry but that's not a valid answer. Sorrrryyyyyyy :(") "
+        print("That's not a valid play. Squeaky is sorry for you.")
     #player was set to True, but we want it to be False so the loop continues
     player = False
     computer = t[randint(0,2)]
+    
